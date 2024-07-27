09c9282f1dc59b924fa57a6950fa542d57bbf818
ldco2016 <renaissance.scholar2012@gmail.com>
2024-07-26 21:12:12 -0500

    caching dynamic routes

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -15,9 +16,7 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
   };
 
-  async function editSnippet() {
-    
-  }
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -29,6 +28,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
