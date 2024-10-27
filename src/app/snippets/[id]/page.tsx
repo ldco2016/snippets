b0513e6cf94962458876d60690f563442211a13d
ldco2016 <renaissance.scholar2012@gmail.com>
2024-10-27 08:31:19 -0500

    Updated Async Dynamic Params in NextJS 15

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,15 +2,27 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
 
+// interface SnippetShowPageProps {
+//   params: {
+//     id: string;
+//   };
+// }
+
+// We also need to update the interface and wrap the params in a Promise:
 interface SnippetShowPageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  // const snippet = await db.snippet.findFirst({
+  //   where: { id: parseInt(props.params.id) },
+  // });
+
+  // In NextJS 15 we must await params or searchParams before accessing
+  const { id } = await props.params;
+
   const snippet = await db.snippet.findFirst({
-    where: { id: parseInt(props.params.id) },
+    where: { id: parseInt(id) },
   });
 
   if (!snippet) {
